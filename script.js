871339f5fb91db6277c5653f70ca6e9d6991889f
kaoutar laajil <kawtarlaajil7@gmail.com>
2024-10-29 21:26:18 +0100

    fctAjouterTache1

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 var cont=0
-function ajouter()
-{
-   var div=document.createElement("div");
-   var input =document.createElement("input");
-   
-
-  //  Ajouter une classe à la div
-    div.classList.add("contentToDo");
-    //  aselectionner element parent de div
-    var parent=document.getElementsByClassName("content")[0];
-    //afectter enfent a son parent
-    parent.appendChild(div);
-    input.type="text";
-     div.appendChild(input);
-      cont++;
-    document.getElementById("cont").textContent=cont;
-}
-
-
 
 function ajouter2()
 {  
-   var modal=document.getElementById("fenetre-medial");
-   
-  //  var ajouterTache=document.getElementById("ajouter");
-   
-   var closeBtn = document.getElementsByClassName("close")[0];
-   
-   
+   let modal=document.getElementById("fenetre-medial");
+   let closeBtn = document.getElementsByClassName("close")[0];
     modal.style.display="block";
-   
-   
+
    closeBtn.onclik=function(){
     modal.style.display="none"
    }
    
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  const ajouterTacheButton = document.getElementById("ajouterTache");
+  ajouterTacheButton.addEventListener("click", ajouterTache);
+});
+ function ajouterTache()
+  {
+    var div=document.createElement("div");
+    var label =document.createElement("label");
+
+    var titre=document.getElementById("titre").value;
+      //  Ajouter une classe à la div
+      div.classList.add("titre");
+
+       //  aselectionner element parent de div
+    var parent=document.getElementsByClassName("content")[0];
+    //afectter enfent a son parent
+    parent.appendChild(div);
+    label.innerHTML= titre;
+    
+     div.appendChild(label);
+      cont++;
+    document.getElementById("cont").textContent=cont;
+    
+  }
+
 function remove(){
   
 }

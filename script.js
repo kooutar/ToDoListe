22909c1e90c0f210dd0e91b06b9312bb3a13d69b
kaoutar laajil <kawtarlaajil7@gmail.com>
2024-10-30 11:27:09 +0100

    fctAjoueSucce

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
 var cont=0
 
+// fct pour afficher modale
 function ajouter2()
 {  
    let modal=document.getElementById("fenetre-medial");
    let closeBtn = document.getElementsByClassName("close")[0];
-    modal.style.display="block";
-
+   modal.style.display="block";
    closeBtn.onclik=function(){
     modal.style.display="none"
    }
-   
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  const ajouterTacheButton = document.getElementById("ajouterTache");
-  ajouterTacheButton.addEventListener("click", ajouterTache);
+// lisnter pour fermer modale
+
+document.addEventListener("DOMContentLoaded", ()=> {
+  var ajouterTacheButton = document.getElementById("close");
+  ajouterTacheButton.addEventListener("click", ()=>{
+   let modal=document.getElementById("fenetre-medial");
+    modal.style.display="none";
+  });
 });
+
+// fct pour ajouter une tache 
  function ajouterTache()
   {
     var div=document.createElement("div");
+    // pour le style 
+    div.classList.add("containerTache");
     var label =document.createElement("label");
-
     var titre=document.getElementById("titre").value;
-      //  Ajouter une classe à la div
-      div.classList.add("titre");
-
-       //  aselectionner element parent de div
+  if(titre==="titre"){
+   alert("vide")
+  }else{
+    //  aselectionner element parent de div
     var parent=document.getElementsByClassName("content")[0];
     //afectter enfent a son parent
     parent.appendChild(div);
     label.innerHTML= titre;
-    
-     div.appendChild(label);
+      div.appendChild(label);
       cont++;
     document.getElementById("cont").textContent=cont;
-    
+}
   }
+    
+  
 
 function remove(){
   
